5a5f4229f69c17712eaac956b8b5f601016f764a
Abdullah <aabdullahaslam91@gmail.com>
2024-10-25 22:01:59 +0500

    26 to 30 Chapters

---
+++ app.js
@@ -0,0 +1,79 @@
+// QUESTION 1 START
+
+// let decimalValue = prompt("Write any Number or Decimal Number")
+
+// alert("Number" + " " + decimalValue)
+// alert("Round Off" + " " + Math.round(decimalValue))
+// alert("Floor Value" + " " + Math.floor(decimalValue))
+// alert("Ceil Value" + " " + Math.ceil(decimalValue))
+
+// QUESTION 1 END
+
+// QUESTION 2 START
+
+// let decimalValue = prompt("Write any Negative Number or Decimal Number")
+
+// alert("Number" + " " + decimalValue)
+// alert("Round Off" + " " + Math.round(decimalValue))
+// alert("Floor Value" + " " + Math.floor(decimalValue))
+// alert("Ceil Value" + " " + Math.ceil(decimalValue))
+
+// QUESTION 2 END
+
+// QUESTION 3 START
+
+// var absoluteValue = -4
+
+// alert(Math.abs(absoluteValue))
+
+// QUESTION 3 END
+
+// QUESTION 4 START
+
+// var diceValues = Math.floor(Math.random()* 6) + 1 
+
+// console.log(diceValues)
+
+
+// QUESTION 4 END
+
+// QUESTION 5 START
+
+// var diceValues = Math.floor(Math.random()* 2) + 1 
+
+// if(diceValues === 1){
+//     alert("Tails")
+// }else{
+//     alert("Heads")
+// }
+
+// QUESTION 5 END
+
+// QUESTION 6 START
+
+// var diceValues = Math.floor(Math.random()* 100) + 1 
+// document.write(diceValues)
+
+// QUESTION 6 END
+
+// QUESTION 7 START
+
+// var askWeight = prompt("ENTER YOUR WEIGHT IN KILOGRAMS")
+
+// alert("The weight of user is:" + " " + askWeight)
+
+// QUESTION 7 END
+
+// QUESTION 8 START
+// HIDDEN NUMBER GAME
+
+// var takeInput = prompt("Write any number between 1 to 10")
+// var diceValues = Math.floor(Math.random()* 10) + 1 
+// console.log(diceValues)
+// if(takeInput == diceValues){
+//     alert("Congratulations!!! You win the Game")
+// }else{
+//     alert("Try Again")
+// }
+
+// QUESTION 8 END
